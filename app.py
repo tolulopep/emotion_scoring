cd34cf7add5754bad80c74e4f612360a6e9c3783
Oyekanmi Tolu <oyekanmitolu@gmail.com>
2021-09-13 06:55:47 -0400

    Adding auto creation of tmp folder

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ app.debug = True
 app.register_blueprint(index_page)
 app.register_blueprint(error_page)
 
+# create tmp directory for upload 
+os.makedirs(os.path.dirname(app.instance_path) + '/tmp', exist_ok=True)
+
 # upload configs
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.vtt']
-app.config['UPLOAD_PATH'] = os.path.dirname(app.instance_path) + '/src/tmp'
+app.config['UPLOAD_PATH'] = os.path.dirname(app.instance_path) + '/tmp'
